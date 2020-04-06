83df89142a333d5a70eb408acc9143b8dbc09c6e
grundb <grundb@kth.se>
2020-04-06 02:58:07 +0200

    initial structure

---
+++ app/src/components/App/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+import Home from '../Home'
+
+const App = () =>  {
+  return (
+    <div className="appContainer">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
